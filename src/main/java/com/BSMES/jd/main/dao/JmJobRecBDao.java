c6d3409a60da1d927599f08894de73e400539ce6
1781592343@qq.com <1781592343@qq.com>
2020-10-05 20:47:39 +0800

    十一小小的修改

--- src/main/java/com/BSMES/jd/main/dao/JmJobRecBDao.java
+++ src/main/java/com/BSMES/jd/main/dao/JmJobRecBDao.java
@@ -6,8 +6,12 @@ import com.BSMES.jd.main.dto.JmJobRecBDTO;
 import com.BSMES.jd.main.entity.JmJobRecBEntity;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface JmJobRecBDao extends BaseDao<JmJobRecBEntity> {
+
+    void insertJobRecBs(List<JmJobRecBDTO> jmJobRecBs);
 
     void updateJobRecB(JmJobRecBDTO jmJobRecB);
 
